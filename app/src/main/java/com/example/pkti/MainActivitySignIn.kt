d0e4dda89416bea525c50269652315ce3e0e546c
angelaho18 <angelllaaaaaaaa@gmail.com>
2020-12-25 23:41:48 +0700

    SignIn dan SignUp

---
+++ app/src/main/java/com/example/pkti/MainActivitySignIn.kt
@@ -0,0 +1,11 @@
+package com.example.pkti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivitySignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_sign_in)
+    }
+}

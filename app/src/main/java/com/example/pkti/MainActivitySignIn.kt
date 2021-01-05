cf23af9c31b3230b666a992f28f9b0c348d08d9c
angelaho18 <angelllaaaaaaaa@gmail.com>
2021-01-05 22:11:13 +0700

    Change all - sign up - sign in - search

--- app/src/main/java/com/example/pkti/MainActivitySignIn.kt
+++ app/src/main/java/com/example/pkti/MainActivitySignIn.kt
@@ -1,11 +1,17 @@
 package com.example.pkti
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main_sign_in.*
 
 class MainActivitySignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_sign_in)
+        signup_button2.setOnClickListener {
+            val intent_signup = Intent(this,MainActivitySignUp::class.java)
+            startActivity(intent_signup)
+        }
     }
 }

9a9aac438593e3433a3a0bc609063a3b442ed0b6
angelaho18 <angelllaaaaaaaa@gmail.com>
2021-01-24 12:03:32 +0700

    all

---
+++ app/src/main/java/com/example/pkti/MainActivitySearch.kt
@@ -0,0 +1,11 @@
+package com.example.pkti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivitySearch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_search)
+    }
+}
